e3ab5dde505cd96a70c0926692dae8af069b732a
DuskUmbrella809 <lxgonzalez31@gmail.com>
2026-07-02 18:29:43 -0400

    refactor: create dashboard module structure

--- src/sports/screens/search.py
+++ src/sports/screens/search.py
@@ -1,8 +1,15 @@
 from textual.app import ComposeResult
 from textual.binding import Binding
 from textual.containers import Vertical
+from textual.message import Message
 from textual.screen import ModalScreen
-from textual.widgets import Input, Footer, Header
+from textual.widgets import Footer, Header, Input
+
+
+class SearchSubmitted(Message):
+    def __init__(self, query: str):
+        super().__init__()
+        self.query = query
 
 
 class SearchScreen(ModalScreen):
@@ -18,7 +25,20 @@ class SearchScreen(ModalScreen):
         with Vertical():
 
             yield Input(
-                placeholder="Search for a team, league, player..."
+                placeholder="Search teams or leagues..."
             )
 
         yield Footer()
+
+    def on_input_submitted(
+        self,
+        event: Input.Submitted,
+    ) -> None:
+
+        self.post_message(
+            SearchSubmitted(
+                event.value
+            )
+        )
+
+        self.app.pop_screen()
